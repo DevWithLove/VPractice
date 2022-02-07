e99ea29f18dafc89eaae613e54a0815586fb9bed
Tony Mu <devwithlove2015@gmail.com>
2022-02-07 20:05:16 +1300

    Fixed fastingTimer view  scrolling issue for small device

--- VacasaPractice/SwiftUI/FastingTimer/FastingTimerRootView.swift
+++ VacasaPractice/SwiftUI/FastingTimer/FastingTimerRootView.swift
@@ -30,68 +30,68 @@ struct FastingTimerRootView: View {
             // Go to Edit, Format -> Show Colors to pick color, pick a color and drag drop the color into the color init
             Color(#colorLiteral(red: 0.04420081526, green: 0, blue: 0.0760954842, alpha: 1))
                 .ignoresSafeArea(edges: .top)
-            content
+            
+            ScrollView {
+                content
+            }
         }
     }
     
     var content: some View {
-        ZStack {
-            VStack(spacing: 40) {
-                // MARK: Title
-                Text(title)
-                    .font(.headline)
-                    .foregroundColor(Color(#colorLiteral(red: 0.2863707542, green: 0.5344309807, blue: 1, alpha: 1)))
+        VStack(spacing: 40) {
+            // MARK: Title
+            Text(title)
+                .font(.headline)
+                .foregroundColor(Color(#colorLiteral(red: 0.2863707542, green: 0.5344309807, blue: 1, alpha: 1)))
+            
+            // MARK: Fasting plan
+            Text(fastingManager.fastingPlan.rawValue)
+                .fontWeight(.semibold)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 8)
+                .background(.thinMaterial)
+                .cornerRadius(20)
+            
+            // MARK: Progress ring
+            ProgressRing()
+                .environmentObject(fastingManager)
+            
+            HStack(spacing: 60) {
+                // MARK: Start time
+                VStack(spacing: 5) {
+                    Text(fastingManager.fastingState == .notStarted ? "Start" : "Started")
+                        .opacity(0.7)
+                    
+                    Text(fastingManager.startTime, format: .dateTime.weekday().hour().minute().second())
+                        .fontWeight(.bold)
+                }
                 
-                // MARK: Fasting plan
-                Text(fastingManager.fastingPlan.rawValue)
+                // MARK: End time
+                VStack(spacing: 5) {
+                    Text(fastingManager.fastingState == .notStarted ? "End" : "Ends")
+                        .opacity(0.7)
+                    
+                    Text(fastingManager.endTime, format: .dateTime.weekday().hour().minute().second())
+                        .fontWeight(.bold)
+                }
+            }
+            
+            // MARK: Button
+            Button{
+                fastingManager.toggleFastingState()
+            } label: {
+                Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
+                    .font(.title3)
                     .fontWeight(.semibold)
                     .padding(.horizontal, 24)
                     .padding(.vertical, 8)
                     .background(.thinMaterial)
                     .cornerRadius(20)
-                Spacer()
-            }.padding()
-            
-            VStack(spacing: 40) {
-                // MARK: Progress ring
-                ProgressRing()
-                    .environmentObject(fastingManager)
-            
-                HStack(spacing: 60) {
-                    // MARK: Start time
-                    VStack(spacing: 5) {
-                        Text(fastingManager.fastingState == .notStarted ? "Start" : "Started")
-                            .opacity(0.7)
-                        
-                        Text(fastingManager.startTime, format: .dateTime.weekday().hour().minute().second())
-                            .fontWeight(.bold)
-                    }
-                    
-                    // MARK: End time
-                    VStack(spacing: 5) {
-                        Text(fastingManager.fastingState == .notStarted ? "End" : "Ends")
-                            .opacity(0.7)
-                        
-                        Text(fastingManager.endTime, format: .dateTime.weekday().hour().minute().second())
-                            .fontWeight(.bold)
-                    }
-                }
-                
-                // MARK: Button
-                Button{
-                    fastingManager.toggleFastingState()
-                } label: {
-                    Text(fastingManager.fastingState == .fasting ? "End fast" : "Start fasting")
-                        .font(.title3)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 24)
-                        .padding(.vertical, 8)
-                        .background(.thinMaterial)
-                        .cornerRadius(20)
-                }
             }
-            .padding()
+            
+            Spacer()
         }
+        .padding()
         .foregroundColor(.white)
     }
 }
